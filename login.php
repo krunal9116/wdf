2dd42a4e7bc846bf855a71bbe20010a1cb429cab
krunal9116 <krunalvaghamshi8@gmail.com>
2025-10-08 01:13:06 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Login</title>
-<link rel="stylesheet" href="css/style.css"></head>
+<link rel="stylesheet" href="style.css"></head>
 <body>
 <div class="container">
   <h2>Login</h2>
@@ -12,3 +12,4 @@
   </form>
 </div>
 </body></html>
+
